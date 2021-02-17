0a25701944a8d81bfe8d895ba373be6a6430b6f2
Alejandro Upegui Betancur <aub.17@hotmail.com>
2021-02-16 20:48:46 -0500

    [ADD] Navigation for stack

--- navigations/Navigation.jsx
+++ navigations/Navigation.jsx
@@ -1,22 +1,41 @@
 import { NavigationContainer, TabActions } from '@react-navigation/native'
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
 import React from 'react'
-import Restaurants from '../screens/Restaurants'
-import Favorites from '../screens/Favorites'
-import TopRestaurants from '../screens/TopRestaurants'
-import Search from '../screens/Search'
-import Account from '../screens/Account'
-
+import RestaurantsStack from './RestaurantsStack'
+import AccountStack from './AccountStack'
+import FavoritesStack from './FavoritesStack'
+import TopRestaurantstack from './TopRestaurantsStack'
+import SearchStack from './SearchStack'
 const Tab = createBottomTabNavigator()
 export default function Navigation() {
     return (
        <NavigationContainer>
         <Tab.Navigator>
-            <Tab.Screen options={{title:"Restaurants"}} name="restaurants" component={Restaurants}/>
-            <Tab.Screen options={{title:"Favorites"}} name="favorites" component={Favorites}/>
-            <Tab.Screen options={{title:"Top 5"}} name="topRestaurants" component={TopRestaurants}/>
-            <Tab.Screen options={{title:"Search"}} name="search" component={Search}/>
-            <Tab.Screen options={{title:"Account "}} name="account" component={Account}/>
+            <Tab.Screen 
+            options={{title:"Restaurants"}} 
+            name="restaurants" 
+            component={RestaurantsStack}
+            />
+            <Tab.Screen 
+            options={{title:"Favorites"}} 
+            name="favorites" 
+            component={FavoritesStack}
+            />
+            <Tab.Screen 
+            options={{title:"Top 5"}} 
+            name="topRestaurants" 
+            component={TopRestaurantstack}
+            />
+            <Tab.Screen 
+            options={{title:"Search"}} 
+            name="search" 
+            component={SearchStack}
+            />
+            <Tab.Screen 
+            options={{title:"Account "}} 
+            name="account" 
+            component={AccountStack}
+            />
         </Tab.Navigator>
        </NavigationContainer>
     )
---
+++ navigations/SearchStack.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import Search from '../screens/Search'
+export default function SearchStack() {
+    const Stack = createStackNavigator()
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="search" component={Search} 
+            options={{title:"Buscador"}}
+            />
+        </Stack.Navigator>
+    )
+}
